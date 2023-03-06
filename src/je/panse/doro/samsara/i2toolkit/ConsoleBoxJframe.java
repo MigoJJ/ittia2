ecea05475cf93638bb27651332e238d022e7d547
Woon Jung Koh <jjkoh21@gmail.com>
2023-03-06 12:41:07 +0900

    +inputTextField.getText());

--- src/je/panse/doro/samsara/i2toolkit/ConsoleBoxJframe.java
+++ src/je/panse/doro/samsara/i2toolkit/ConsoleBoxJframe.java
@@ -8,29 +8,30 @@ package je.panse.doro.samsara.i2toolkit;
 	    private JTextArea consoleTextArea;
 	    private JTextField inputTextField;
 
-	    public ConsoleBoxJframe() {
+		public ConsoleBoxJframe() {
 			super("Console Box Example");
-			JScrollPane scrollPane = new JScrollPane(consoleTextArea);
 			consoleTextArea = new JTextArea();
 			consoleTextArea.setEditable(false);
-			consoleTextArea.setFont(new Font("Courier", Font.PLAIN,18));
-	        
-	        inputTextField = new JTextField();
-	        inputTextField.setFont(new Font("Courier", Font.PLAIN,18));
-	        
-	        inputTextField.addActionListener(new ActionListener() {
-	            public void actionPerformed(ActionEvent e) {
-	                consoleTextArea.append(">    " + inputTextField.getText() + "\n");
-	                inputTextField.setText("");
-	            }
-	        });
-
-	        add(scrollPane, BorderLayout.CENTER);
-	        add(inputTextField, BorderLayout.SOUTH);
-	        setSize(400, 300);
-	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        setVisible(true);
-	    }
+			consoleTextArea.setFont(new Font("Arial", Font.PLAIN,16));
+			
+			JScrollPane scrollPane = new JScrollPane(consoleTextArea);
+			inputTextField = new JTextField();
+			inputTextField.setFont(new Font("Arial", Font.PLAIN,16));
+			
+			inputTextField.addActionListener(new ActionListener() {
+			    public void actionPerformed(ActionEvent e) {
+			        consoleTextArea.append(">    " + inputTextField.getText() + "\n");
+			    	  System.out.println("inputTextField.getText()" +inputTextField.getText());
+			        inputTextField.setText("");
+			    }
+			});
+			
+			add(scrollPane, BorderLayout.CENTER);
+			add(inputTextField, BorderLayout.SOUTH);
+			setSize(600, 450);
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setVisible(true);
+		}
 
 	    public static void main(String[] args) {
 	    	ConsoleBoxJframe consoleBox = new ConsoleBoxJframe();
