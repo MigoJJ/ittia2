431cf505ad39b04a0a099943451d0628d60e2e35
Woon Jung Koh <jjkoh21@gmail.com>
2023-03-08 16:09:39 +0900

    https://github.com/MigoJJ/ittia2.git

--- src/je/panse/doro/samsara/i2toolkit/exercise/ExampleFrame.java
+++ src/je/panse/doro/samsara/i2toolkit/exercise/ExampleFrame.java
@@ -1,43 +1,41 @@
 package je.panse.doro.samsara.i2toolkit.exercise;
 
 import javax.swing.*;
-
 import je.panse.doro.singlebeam.i2datafilescombobox.ReturnPages;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ExampleFrame extends JFrame {
-
     public ExampleFrame() {
-        setTitle("Example JFrame");
+        setTitle("GDS EMR Interface");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1800, 1600/12*9);
+        setLocationRelativeTo(null);
 
-        JPanel panel1 = createPanel("Panel 1 starting");
-        JPanel panel2 = createPanel("Panel 2");
-        JPanel panel3 = createPanel("Panel 3");
-        JPanel panel4 = createPanel("Panel 4");
+        JPanel panel1 = createPanel("CCPIROS");
+        JPanel panel2 = createPanel("PMHSUJOBJ");
+        JPanel panel3 = createPanel("ASSPLAN");
+        JPanel panel4 = createPanel("OTHER");
 
         setLayout(new GridLayout(2, 2));
         add(panel1);
         add(panel2);
         add(panel3);
         add(panel4);
-
+               
         setVisible(true);
     }
 
     private JPanel createPanel(String panelName) {
 			JPanel panel = new JPanel(new BorderLayout());
-			panel.setBorder(BorderFactory.createTitledBorder(panelName));
+//			panel.setBorder(BorderFactory.createTitledBorder(panelName));
 			
 			JLabel label = new JLabel(panelName);
-			//        panel.add(label, BorderLayout.NORTH);
-			
+			panel.add(label, BorderLayout.NORTH);
+
 			JTextField textField = new JTextField(20);
-			JTextArea textArea = new JTextArea(28, 9);
+			JTextArea textArea = new JTextArea(20, 9);
 			JScrollPane scrollPane = new JScrollPane(textArea);
 			String returnedStr= ReturnPages.startPageText();
 
@@ -45,13 +43,20 @@ public class ExampleFrame extends JFrame {
 			textArea.setWrapStyleWord(true);
 			textArea.setEditable(false);
 			textArea.append(returnedStr);
+			
 			textField.addActionListener(new ActionListener() {
 			    public void actionPerformed(ActionEvent e) {
-			        textArea.append(">    " + textField.getText() + "\n");
-			    textArea.setCaretPosition(textArea.getDocument().getLength());
-			    textField.setText("");
+					textArea.append(">    " + textField.getText() + "\n");
+					textArea.setCaretPosition(textArea.getDocument().getLength());
+					textField.setText("");
 			    }
 			});
+
+			Font font = new Font("Consolas", Font.PLAIN, 16);
+				textField.setFont(font);
+				textArea.setFont(font);
+				panel.setFont(font);
+				label.setFont(font); 
 			panel.add(textField, BorderLayout.CENTER);
 			panel.add(scrollPane, BorderLayout.SOUTH);
         return panel;
--- src/je/panse/doro/singlebeam/i2datafilescombobox/ReturnPages.java
+++ src/je/panse/doro/singlebeam/i2datafilescombobox/ReturnPages.java
@@ -2,13 +2,24 @@ package je.panse.doro.singlebeam.i2datafilescombobox;
 
 public class ReturnPages {
 	public static String startPageText() {
-		String text = "[Startup Page]\n" +
-                  "- -------------------------------\n" +
+		String text = "\t[Startup Page]\n" +
+                  "\t--------------------------------\n" +
                   "\t[ 1 ] New Patient\n" +
                   "\t[ 2 ] SOAP\n" +
                   "\t[ 3 ] Disease Category\n" +
                   "\t[ 4 ] Item\n" +
-                  "-------------------------------\n";
+                  "\t-------------------------------\n";
+    return text;
+	}
+	
+	public static String startPageTextCategory() {
+		String text = "\t[Startup Page]\n" +
+                  "\t--------------------------------\n" +
+                  "\t[ 1 ] New Patient\n" +
+                  "\t[ 2 ] SOAP\n" +
+                  "\t[ 3 ] Disease Category\n" +
+                  "\t[ 4 ] Item\n" +
+                  "\t-------------------------------\n";
     return text;
 	}
 }
